82a00e5d11a1c7223d8efda53385296a953f9dab
Nivetha S R <nivetha20niv@gmail.com>
2022-05-29 21:08:56 +0530

    integratedinconsole

--- clientapp/src/App.js
+++ clientapp/src/App.js
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router";
 //import Loader from "./layout/Loader";
 import Home from './components/Home'
+import Login from "./components/auth/Login";
 
 const App = () => {
   return ( 
     <div>
       <Routes>
         <Route path="/" element={<Home/>}  />
+        <Route path="/login" element={<Login/>}  />
       </Routes>
     </div>
    );
